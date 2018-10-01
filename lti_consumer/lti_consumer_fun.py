43a7489825b5c9cdee14f31059d5b9a470b08546
Richard Moch <richard.moch@gmail.com>
2018-10-01 08:35:31 +0200

    Retrie configuration from Django settings

--- lti_consumer/lti_consumer_fun.py
+++ lti_consumer/lti_consumer_fun.py
@@ -15,8 +15,30 @@ class FUNLtiConsumerXBlockBase(LtiConsumerXBlock):
     display_name = "I'm a FUN xblock"
     description = "I'm a FUN xblock's description"
     def __init__(self, *args, **kwargs):
-        self.__class__.XBLOCKS_FACTORY = getattr(settings, "XBLOCKS_FACTORY")
         super(FUNLtiConsumerXBlockBase, self).__init__(*args, **kwargs)
+        self.__class__.XBLOCKS_FACTORY = getattr(settings, "XBLOCKS_FACTORY")
+        self.block_type = kwargs["scope_ids"].usage_id.block_type  # Hi, this is my name (ie: lti_consumer_Video)
+
+        self.display_name = "TEST"
+
+    def get_configuration(self):
+        """
+        Retrieving component configuration from Django settings
+        """
+        for component in self.__class__.XBLOCKS_FACTORY["components"]:
+            if self.block_type.startswith(component["module"]):
+                suffix = self.block_type[len(component["module"]) + 1:]
+                conf = [subclass for subclass in component["subclasses"] if subclass["suffix"] == suffix][0]
+                return conf
+
+    @property
+    def lti_provider_key_secret(self):
+        """
+        Override parent's method to use credentials from Django settings instead
+        of courses settings
+        """
+        configuration = self.get_configuration()
+        return configuration["lti"]["key"], configuration["lti"]["secret"]
 
 
     #def studio_view(self, context):

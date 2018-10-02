44c6643ae4782862d49f6f460f7ee46ebe720e80
Richard Moch <richard.moch@gmail.com>
2018-10-02 14:47:55 +0000

    wip

--- lti_consumer/lti_consumer_fun.py
+++ lti_consumer/lti_consumer_fun.py
@@ -10,16 +10,38 @@ from xblock.utils import component_factory
 
 logger = logging.getLogger('__name__')
 
+# https://groups.google.com/forum/#!topic/edx-code/3hQVXPd1sRM
+
+#editable_fields = (
+#'display_name', 'description', 'lti_id', 'launch_url', 'custom_parameters', 'launch_target', 'button_text',
+#'inline_height', 'modal_height', 'modal_width', 'has_score', 'weight', 'hide_launch', 'accept_grades_past_due',
+#'ask_to_send_username', 'ask_to_send_email'
+#)
+
 
 class FUNLtiConsumerXBlockBase(LtiConsumerXBlock):
-    display_name = "I'm a FUN xblock"
-    description = "I'm a FUN xblock's description"
     def __init__(self, *args, **kwargs):
-        super(FUNLtiConsumerXBlockBase, self).__init__(*args, **kwargs)
         self.__class__.XBLOCKS_FACTORY = getattr(settings, "XBLOCKS_FACTORY")
         self.block_type = kwargs["scope_ids"].usage_id.block_type  # Hi, this is my name (ie: lti_consumer_Video)
+        super(FUNLtiConsumerXBlockBase, self).__init__(*args, **kwargs)
+        import ipdb; ipdb.set_trace()
+        self.editable_fields = list(self.editable_fields)
 
-        self.display_name = "TEST"
+        self.set_defaults()
+
+    @property
+    def _user_is_staff(self):
+        return getattr(self.runtime, 'user_is_staff', False)
+
+    def set_defaults(self):
+        """
+        Set default values for configured fields and remove from editable list
+        """
+        for field, value in self.get_configuration()['default_values'].items():
+            if not self.fields[field].is_set_on(self):
+                self.fields[field].write_to(self, value)
+            if not self._user_is_staff:
+                self.editable_fields.pop(self.editable_fields.index(field))
 
     def get_configuration(self):
         """
@@ -34,26 +56,22 @@ class FUNLtiConsumerXBlockBase(LtiConsumerXBlock):
     @property
     def lti_provider_key_secret(self):
         """
-        Override parent's method to use credentials from Django settings instead
-        of courses settings
+        Override parent's method to use credentials from Django settings if available
+        instead of courses settings
         """
         configuration = self.get_configuration()
-        return configuration["lti"]["key"], configuration["lti"]["secret"]
+        if "lti" in configuration:
+            return configuration["lti"]["key"], configuration["lti"]["secret"]
+        return super(FUNLtiConsumerXBlockBase, self).lti_provider_key_secret
 
 
-    #def studio_view(self, context):
-    #    """
-    #    Create a fragment used to display the edit view in the Studio.
-    #    """
-#
-#
-#
-    #    context.update({
-    #        "self": self
-    #    })
-#
-    #    #fragment = Fragment()
-    #    #fragment.add_content(loader.render_template("static/html/cohortxblock_edit.html",context))
-    #    #fragment.add_javascript(self.resource_string("static/js/src/cohortxblock_edit.js"))
-    #    #fragment.initialize_js('CohortXBlockEdit')
-    #    return fragment
+    def studio_view(self, context):
+        #self.editable_fields = ['lti_id', 'launch_url', ]
+        #if not self.lti_id:
+        #    self.lti_id = "LTI ID test"
+
+        return super(FUNLtiConsumerXBlockBase, self).studio_view(context)
+
+
+    #def author_view(self, context):
+    #    return super(FUNLtiConsumerXBlockBase, self).author_view()

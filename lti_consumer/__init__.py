efbf15fb2076c2962259ef237f9198bdf0449848
Richard Moch <richard.moch@gmail.com>
2018-10-03 17:50:01 +0000

    refactr without factory, using get_template instead

--- lti_consumer/__init__.py
+++ lti_consumer/__init__.py
@@ -2,7 +2,5 @@
 Runtime will load the XBlock class from here.
 """
 
-from django.conf import settings
-
-from .lti_consumer import LtiConsumerXBlock
 from .lti_consumer_fun import FUNLtiConsumerXBlockBase
+from .lti_consumer_fun import add_dynamic_components
--- lti_consumer/lti_consumer_fun.py
+++ lti_consumer/lti_consumer_fun.py
@@ -5,53 +5,47 @@ from .lti_consumer import LtiConsumerXBlock
 
 from django.conf import settings
 
-from xblock.utils import component_factory
-
 
 logger = logging.getLogger('__name__')
-
-# https://groups.google.com/forum/#!topic/edx-code/3hQVXPd1sRM
-
-#editable_fields = (
-#'display_name', 'description', 'lti_id', 'launch_url', 'custom_parameters', 'launch_target', 'button_text',
-#'inline_height', 'modal_height', 'modal_width', 'has_score', 'weight', 'hide_launch', 'accept_grades_past_due',
-#'ask_to_send_username', 'ask_to_send_email'
-#)
 
 
 class FUNLtiConsumerXBlockBase(LtiConsumerXBlock):
     def __init__(self, *args, **kwargs):
-        self.__class__.XBLOCKS_FACTORY = getattr(settings, "XBLOCKS_FACTORY")
-        self.block_type = kwargs["scope_ids"].usage_id.block_type  # Hi, this is my name (ie: lti_consumer_Video)
         super(FUNLtiConsumerXBlockBase, self).__init__(*args, **kwargs)
-        import ipdb; ipdb.set_trace()
+
+        self.set_editable_fields()
+
+    @classmethod
+    def get_template(cls, boilerplate):
+        """
+        Returns a dict that will be used by modulestore to initialize xblock fields
+        """
+        template = {}
+        template['metadata'] = get_configuration()["default_values"]
+        return template
+
+
+    def set_editable_fields(self):
+        """
+        Remove from editable_fields list fields that we already have configured
+        """
         self.editable_fields = list(self.editable_fields)
-
-        self.set_defaults()
-
-    @property
-    def _user_is_staff(self):
-        return getattr(self.runtime, 'user_is_staff', False)
-
-    def set_defaults(self):
-        """
-        Set default values for configured fields and remove from editable list
-        """
         for field, value in self.get_configuration()['default_values'].items():
-            if not self.fields[field].is_set_on(self):
-                self.fields[field].write_to(self, value)
-            if not self._user_is_staff:
-                self.editable_fields.pop(self.editable_fields.index(field))
+            self.editable_fields.pop(self.editable_fields.index(field))
 
     def get_configuration(self):
         """
         Retrieving component configuration from Django settings
         """
-        for component in self.__class__.XBLOCKS_FACTORY["components"]:
-            if self.block_type.startswith(component["module"]):
-                suffix = self.block_type[len(component["module"]) + 1:]
-                conf = [subclass for subclass in component["subclasses"] if subclass["suffix"] == suffix][0]
-                return conf
+        name = self.description
+        for component in settings.XBLOCKS_FACTORY["components"]:
+            if component["module"] == "lti_consumer":
+                conf = [
+                    subclass for subclass in component["subclasses"]
+                    if subclass["name"] == name]
+                if conf:
+                    return conf[0]
+        return None
 
     @property
     def lti_provider_key_secret(self):
@@ -65,13 +59,23 @@ class FUNLtiConsumerXBlockBase(LtiConsumerXBlock):
         return super(FUNLtiConsumerXBlockBase, self).lti_provider_key_secret
 
 
-    def studio_view(self, context):
-        #self.editable_fields = ['lti_id', 'launch_url', ]
-        #if not self.lti_id:
-        #    self.lti_id = "LTI ID test"
-
-        return super(FUNLtiConsumerXBlockBase, self).studio_view(context)
-
-
-    #def author_view(self, context):
-    #    return super(FUNLtiConsumerXBlockBase, self).author_view()
+def add_dynamic_components(XBLOCKS_FACTORY, advanced_component_templates,
+    categories, create_template_dict, course_advanced_keys):
+    """
+    Reads XBLOCKS_FACTORY configuration to create related button in studio
+    to instanciate configured xblocks
+    """
+    if XBLOCKS_FACTORY:
+        for component in XBLOCKS_FACTORY['components']:
+            for class_info in component["subclasses"]:
+                display_name = class_info["display"]
+                key = component["module"] + "_" + class_info["name"]
+                advanced_component_templates['templates'].append(
+                    create_template_dict(
+                        name=display_name,
+                        cat=component["module"],
+                        boilerplate_name=class_info["name"]
+                        )
+                )
+            # Remove original overrided component
+            course_advanced_keys.pop(course_advanced_keys.index(component['module']))
